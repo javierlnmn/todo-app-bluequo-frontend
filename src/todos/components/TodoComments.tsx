0cfe09547792aac4bae2bb051ff7488d090ceb50
javierlnmn <jlm03123@gmail.com>
2025-03-21 11:04:45 +0100

    Add delete comment animation

--- src/todos/components/TodoComments.tsx
+++ src/todos/components/TodoComments.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import { useUserStore } from '@auth/stores/userStore';
 import { isOwnerOrAdmin } from '@auth/utils/user';
@@ -45,36 +46,45 @@ const TodoComments: FC<TodoCommentsProps> = ({ comments }) => {
     };
 
     return (
-        <div className='flex flex-col gap-2'>
+        <div className="flex flex-col gap-2">
             <h3 className="text-xl font-bold">Comments</h3>
             {commentList.length > 0 ? (
-                <div className='flex flex-col gap-3'>
-                    {commentList.map((comment) => (
-                        <div key={comment.id} className='bg-zinc-200 dark:bg-zinc-700 rounded-md p-3 py-2 flex flex-col gap-2'>
-                            <div className='flex justify-between items-center gap-3'>
-                                <div className='relative'>
-                                    <p className='font-bold'>{comment.user.username}</p>
-                                    {comment.user.isSuperuser && (
-                                        <div className='absolute top-0 -right-2 rotate-[35deg]'>
-                                            <CrownIcon className='w-3 h-3 dark:fill-yellow-400 dark:stroke-yellow-400 fill-yellow-600 stroke-yellow-600' />
-                                        </div>
-                                    )}
+                <div className="flex flex-col gap-3">
+                    <AnimatePresence>
+                        {commentList.map((comment) => (
+                            <motion.div
+                                layout
+                                key={comment.id}
+                                className="bg-zinc-200 dark:bg-zinc-700 rounded-md p-3 py-2 flex flex-col gap-2"
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0, x: -20 }}
+                                transition={{ duration: 0.2 }}
+                            >
+                                <div className="flex justify-between items-center gap-3">
+                                    <div className="relative">
+                                        <p className="font-bold">{comment.user.username}</p>
+                                        {comment.user.isSuperuser && (
+                                            <div className="absolute top-0 -right-2 rotate-[35deg]">
+                                                <CrownIcon className="w-3 h-3 dark:fill-yellow-400 dark:stroke-yellow-400 fill-yellow-600 stroke-yellow-600" />
+                                            </div>
+                                        )}
+                                    </div>
+                                    <div className="flex gap-3 items-center">
+                                        {isOwnerOrAdmin(isSuperuser, username, comment.user.username) && (
+                                            <button onClick={() => handleDeleteComment(comment.id)}>
+                                                <TrashIcon className="w-4 h-4 cursor-pointer hover:text-red-500 transition-colors" />
+                                            </button>
+                                        )}
+                                        <p className="font-light opacity-50 text-sm">{formatDate(comment.created)}</p>
+                                    </div>
                                 </div>
-                                <div className='flex gap-3 items-center'>
-                                    {isOwnerOrAdmin(isSuperuser, username, comment.user.username) && (
-                                        <button onClick={() => handleDeleteComment(comment.id)}>
-                                            <TrashIcon className='w-4 h-4 cursor-pointer hover:text-red-500 transition-colors' />
-                                        </button>
-                                    )}
-                                    <p className='font-light opacity-50 text-sm'>{formatDate(comment.created)}</p>
-                                </div>
-                            </div>
-                            <p>{comment.content}</p>
-                        </div>
-                    ))}
+                                <p>{comment.content}</p>
+                            </motion.div>
+                        ))}
+                    </AnimatePresence>
                 </div>
             ) : (
-                <p className='font-light opacity-70'>There are no comments yet.</p>
+                <p className="font-light opacity-70">There are no comments yet.</p>
             )}
         </div>
     );
